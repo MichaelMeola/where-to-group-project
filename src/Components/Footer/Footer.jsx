024c39e878b87b477f097f50f28d6db976981a02
Richard Webber <rikki.webber@gmail.com>
2024-01-22 16:56:53 -0700

    Building out App.jsx and main.jsx with respected routes

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className='container-fluid font-small pt-4'>
+        <div className='row'>
+          
+          <div className='col-md-3 mt-md-0 mt-3'>
+            <ul className='list-unstyled'>
+              <li><a className='footer-link' href="">About</a></li>
+            </ul>
+          </div>
+
+          <div>
+            
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  )
+}
