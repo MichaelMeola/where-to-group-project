b0497113370ed08b75c6e2064cdfcdf6e23b0d1a
Michael Meola <meolaadonai@gmail.com>
2024-01-23 12:46:33 -0700

    more handler functions done

--- server/controller.js
+++ server/controller.js
@@ -1,1 +1,26 @@
-import { User, Group, Event, Winner } from "./models.js"
+import { User, Group, Event, Winner } from "./db/models.js"
+
+const handlerFunctions = {
+
+    getUsers: async(req, res) => {
+        const allUsers = await User.findAll()
+        res.send(allUsers)
+    },
+
+    getGroups: async(req, res) => {
+        const allGroups = await Group.findAll()
+        res.send(allGroups)
+    },
+
+    getEvents: async(req, res) => {
+        const allEvents = await Event.findAll()
+        res.send(allEvents)
+    },
+
+    getWinners: async(req, res) => {
+        const allWinners = await Winner.findAll()
+        res.send(allWinners)
+    },
+}
+
+export default handlerFunctions
--- server/index.js
+++ server/index.js
@@ -9,4 +9,13 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
+import handlerFunctions from './controller.js';
+
+const { getUsers, getGroups, getEvents, getWinners } = handlerFunctions
+
+app.get('/api/users', getUsers)
+app.get('/api/groups', getGroups)
+app.get('/api/events', getEvents)
+app.get('/api/winners', getWinners)
+
 ViteExpress.listen(app, 9999, () => console.log(`Server running on http://localhost:9999`));
