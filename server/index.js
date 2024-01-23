337a413ae03b641493aa5ec310cbb48625647567
Michael Meola <meolaadonai@gmail.com>
2024-01-23 14:41:56 -0700

    same again

--- server/index.js
+++ server/index.js
@@ -2,16 +2,16 @@ import express from "express";
 import morgan from "morgan";
 import ViteExpress from "vite-express";
 import handlerFunctions from "./controller.js";
+
 const app = express();
-const { register } = handlerFunctions;
+
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
-import handlerFunctions from "./controller.js";
-
-const { getUsers, getGroups, getEvents, getWinners } = handlerFunctions;
+const { getUsers, getGroups, getEvents, getWinners, register } =
+  handlerFunctions;
 
 app.get("/api/users", getUsers);
 app.get("/api/groups", getGroups);
