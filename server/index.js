4182bd245ff4851f10a25e27dc73b5482ca57b39
Michael Meola <meolaadonai@gmail.com>
2024-01-23 14:31:55 -0700

    added age to model.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,24 @@
-import express from 'express';
-import morgan from 'morgan';
-import ViteExpress from 'vite-express';
-import handlerFunctions from './controller.js';
+import express from "express";
+import morgan from "morgan";
+import ViteExpress from "vite-express";
+import handlerFunctions from "./controller.js";
 const app = express();
 const { register } = handlerFunctions;
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-import handlerFunctions from './controller.js';
+import handlerFunctions from "./controller.js";
 
-const { getUsers, getGroups, getEvents, getWinners } = handlerFunctions
+const { getUsers, getGroups, getEvents, getWinners } = handlerFunctions;
 
-app.get('/api/users', getUsers)
-app.get('/api/groups', getGroups)
-app.get('/api/events', getEvents)
-app.get('/api/winners', getWinners)
-app.post('/register', register);
+app.get("/api/users", getUsers);
+app.get("/api/groups", getGroups);
+app.get("/api/events", getEvents);
+app.get("/api/winners", getWinners);
+app.post("/register", register);
 
-ViteExpress.listen(app, 9999, () => console.log(`Server running on http://localhost:9999`));
+ViteExpress.listen(app, 9999, () =>
+  console.log(`Server running on http://localhost:9999`)
+);
