93783a5106cd7fe6a3f2ba454588cf75c1c8a522
Michael Meola <meolaadonai@gmail.com>
2024-01-26 09:44:40 -0700

    merged rikkis and mine

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -1,25 +1,15 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { useEventsStore, useProfileStore } from "../../globalState.jsx";
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const Events = () => {
   const { events, setEvents } = useEventsStore();
+  const { profile } = useProfileStore()
 
   useEffect(() => {
     axios
