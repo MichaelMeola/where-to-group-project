044b92a5974e493fe4a6a34e3a8538e6e7699204
Michael Meola <meolaadonai@gmail.com>
2024-01-29 13:51:48 -0700

    select event by date and likes displaying

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useEventsStore, useProfileStore } from "../../globalState.jsx";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
-import { Container } from "@mui/material";
+import { Container, Box } from "@mui/material";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
@@ -14,11 +14,16 @@ import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router-dom";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import Grid from "@mui/material/Grid";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 const Events = () => {
   const navigate = useNavigate();
   const { events, setEvents } = useEventsStore();
   const { profile } = useProfileStore();
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [filteredEvents, setFilteredEvents] = useState([]);
 
   useEffect(() => {
     axios
@@ -29,58 +34,91 @@ const Events = () => {
       .catch((error) => {
         console.log(error);
       });
+
+    if (!profile.username) {
+      navigate("/");
+    }
   }, []);
 
   useEffect(() => {
-    if(!profile.username){
-      navigate('/')
+    if (selectedDate) {
+      const filteredEvents = events.filter(
+        (event) =>
+          new Date(event.date).toLocaleDateString() ===
+          selectedDate.$d.toLocaleDateString()
+      );
+      setFilteredEvents(filteredEvents);
+    } else {
+      setFilteredEvents(events);
     }
-  }, []);
-  console.log(profile);
+  }, [selectedDate, events]);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
   return (
-    <Container sx={{ py: 8 }} maxWidth="md">
-    <Grid container spacing={4}>
-      {events.map((event) => (
-        <Grid item key={event.eventId} xs={12} sm={6} md={4}>
-          <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-            <CardHeader
-              avatar={<Avatar></Avatar>}
-              sx={{ height: "60px" }}
-              title={`Host: ${event.createdBy}`}
-            />
-            <CardMedia
-              component="img"
-              height="200"
-              image={`${event.image}`}
-              alt="Event Image"
-            />
-            <CardContent>
-              <Typography variant="body1" color="text.primary">
-                {event.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {event.address}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {new Date(event.date).toLocaleString("en-US", {
-                  dateStyle: "long",
-                  timeStyle: "short",
-                })}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {event.description}
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-            </CardActions>
-          </Card>
+    <>
+      <Box display="flex" justifyContent="center" paddingTop={8}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Container components={["DatePicker"]}>
+            <DatePicker label="Event Date" onChange={handleDateChange} sx={{ bgcolor: 'white'}}/>
+          </Container>
+        </LocalizationProvider>
+      </Box>
+      <Container sx={{ py: 3 }} maxWidth="md">
+        <Grid container spacing={4}>
+          {filteredEvents.map((event) => (
+            <Grid item key={event.eventId} xs={12} sm={6} md={4}>
+              <Card
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <CardHeader
+                  avatar={<Avatar></Avatar>}
+                  sx={{ height: "60px" }}
+                  title={`Host: @${event.createdBy}`}
+                />
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image={`${event.image}`}
+                  alt="Event Image"
+                />
+                <CardContent>
+                  <Typography variant="body1" color="text.primary">
+                    {event.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {event.address}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {new Date(event.date).toLocaleString("en-US", {
+                      dateStyle: "long",
+                      timeStyle: "short",
+                    })}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {event.description}
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                  </IconButton>
+                  <Typography variant="body2" color="text.secondary">
+                    {event.likes}
+                  </Typography>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
         </Grid>
-      ))}
-    </Grid>
-    </Container>
+      </Container>
+    </>
   );
 };
 
